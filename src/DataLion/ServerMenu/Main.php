1b1f88e153b61d61655782e6279614803db7bf91
Abedalaziz1 <56210541+Abedalaziz1@users.noreply.github.com>
2020-01-29 19:18:18 +1000

    you're welcome :)

--- src/DataLion/ServerMenu/Main.php
+++ src/DataLion/ServerMenu/Main.php
@@ -1,13 +1,7 @@
 <?php
-
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace DataLion\ServerMenu;
-
-
-
-
-
 
 use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\Player;
@@ -16,99 +10,96 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase{
+class Main extends PluginBase
+{
 
     private $config;
 
+    public function onEnable():
+        void
+        {
+            $this->getLogger()
+                ->info("Enabled");
 
-	public function onEnable() : void{
-		$this->getLogger()->info("Enabled");
+            $standard_vals = ["servers" => [["name" => "LocalHost", "ip" => "127.0.0.1", "port" => "19132"], ["name" => "Example Server", "ip" => "play.example.com", "port" => "19132"], ], ];
 
+            $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, $standard_vals);
 
-		$standard_vals = [
-		    "servers" => [
-		        [
-		            "name" => "LocalHost",
-                    "ip" => "127.0.0.1",
-                    "port" => "19132"
-                ],
-                [
-                    "name" => "Example Server",
-                    "ip" => "play.example.com",
-                    "port" => "19132"
-                ],
-            ],
-        ];
-
-
-
-		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, $standard_vals);
-
-
-
-	}
-
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		switch($command->getName()){
-			case "servermenu":
-			    if($sender instanceof Player){
-                    $this->serverMenu($sender);
-                }
-
-
-				return true;
-			default:
-				return false;
-		}
-	}
-
-
-
-
-
-
-	public function onDisable() : void{
-		$this->getLogger()->info("Disabled");
-	}
-
-
-
-
-
-
-	public function serverMenu(Player $player): void{
-        $options = array();
-        foreach($this->config->get("servers") as $server){
-            $options[] = $server["name"];
         }
 
-        if(sizeof($options) == 0){
-            $text = "No servers found";
-        }else{
-            $text = "Select Server";
-        }
-        $form = new SimpleForm(function(Player $player, $data){
-            if($data !== null){
-                if(isset($data)){
-                    if (is_int($data)) {
+        public function onCommand(CommandSender $sender, Command $command, string $label, array $args):
+            bool
+            {
+                switch ($command->getName())
+                {
+                    case "servermenu":
+                        if ($sender instanceof Player)
+                        {
+                            $this->serverMenu($sender);
+                        }
 
-
-                        $server = $this->config->get("servers")[$data];
-                        $player->transfer($server["ip"], intval($server["port"]), $server["name"]);
-                        return;
-
-
-                    }
+                        return true;
+                    default:
+                        return false;
                 }
             }
-            return;
-        });
-        $form->setTitle("Server Selection");
-        $form->setContent($text);
-        foreach($this->config->get("servers") as $server){
-            $form->addButton($server["name"]);
-        }
-        $player->sendForm($form);
-        return;
-    }
-}
+
+            public function onDisable():
+                void
+                {
+                    $this->getLogger()
+                        ->info("Disabled");
+                }
+
+                public function serverMenu(Player $player):
+                    void
+                    {
+                        $options = array();
+                        foreach ($this
+                            ->config
+                            ->get("servers") as $server)
+                        {
+                            $options[] = $server["name"];
+                        }
+
+                        if (sizeof($options) == 0)
+                        {
+                            $text = "No servers found";
+                        }
+                        else
+                        {
+                            $text = "Select Server";
+                        }
+                        $form = new SimpleForm(function (Player $player, $data)
+                        {
+                            if ($data !== null)
+                            {
+                                if (isset($data))
+                                {
+                                    if (is_int($data))
+                                    {
+
+                                        $server = $this
+                                            ->config
+                                            ->get("servers") [$data];
+                                        $player->transfer($server["ip"], intval($server["port"]) , $server["name"]);
+                                        return;
+
+                                    }
+                                }
+                            }
+                            return;
+                        });
+                        $form->setTitle("Server Selection");
+                        $form->setContent($text);
+                        foreach ($this
+                            ->config
+                            ->get("servers") as $server)
+                        {
+                            $form->addButton($server["name"]);
+                        }
+                        $player->sendForm($form);
+                        return;
+                    }
+                }
+                
